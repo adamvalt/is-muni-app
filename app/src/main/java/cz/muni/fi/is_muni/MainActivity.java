97f92631e8939a207b05791115a6bf91011988ff
Adam Valt <valt.adam@gmail.com>
2023-10-02 04:25:33 +0200

    fix: bugfixes
    - fixed wrong orientation rotating
    - fixed mailto: and tel: links
    - generalized app name and content url
    - added custom build config
    - added intent filter to open app when clicking on link with app host name
    - added hw acceleration
    - added instant android apk support
    - added support for restoring instance state

--- app/src/main/java/cz/muni/fi/is_muni/MainActivity.java
+++ app/src/main/java/cz/muni/fi/is_muni/MainActivity.java
@@ -2,11 +2,17 @@ package cz.muni.fi.is_muni;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.CookieManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import java.util.Objects;
 
 import im.delight.android.webview.AdvancedWebView;
 
@@ -15,22 +21,27 @@ import im.delight.android.webview.AdvancedWebView;
  * @author Adam Valt
  */
 public class MainActivity extends Activity implements DefaultListener {
-
-    private static final String PAGE_URL = "https://is.muni.cz/auth/";
+    private static final String PAGE_URL = BuildConfig.PAGE_URL;
     private AdvancedWebView mWebView;
     private SwipeRefreshLayout mySwipeRefreshLayout;
+    private static final String webViewStateKey = "webViewState";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mWebView = findViewById(R.id.webview);
+        mWebView = findViewById(R.id.webView);
+        if (savedInstanceState != null) {
+            mWebView.restoreState(Objects.requireNonNull(savedInstanceState.getBundle(webViewStateKey)));
+            return;
+        }
         mWebView.setListener(this, this);
         mWebView.setGeolocationEnabled(false);
-        mWebView.setMixedContentAllowed(false);
+        mWebView.setMixedContentAllowed(false); // ?
         mWebView.setCookiesEnabled(true);
         mWebView.setThirdPartyCookiesEnabled(true);
+        mWebView.setWebViewClient(getCustomWebViewClient());
         mWebView.loadUrl(PAGE_URL);
 
         mySwipeRefreshLayout = this.findViewById(R.id.swipeContainer);
@@ -44,7 +55,7 @@ public class MainActivity extends Activity implements DefaultListener {
 
     @Override
     public void onBackPressed() {
-        if (!mWebView.canGoBack()) { finish(); return;}
+        if (!mWebView.canGoBack()) { finish(); return; }
         mWebView.goBack();
     }
     @Override
@@ -72,4 +83,28 @@ public class MainActivity extends Activity implements DefaultListener {
             Toast.makeText(getApplicationContext(), "Download failed", Toast.LENGTH_LONG).show();
         }
     }
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Bundle bundle = new Bundle();
+        mWebView.saveState(bundle);
+        outState.putBundle(webViewStateKey, bundle);
+    }
+    @NonNull
+    private WebViewClient getCustomWebViewClient(){
+        return new WebViewClient() {
+            // Fixing problem with mailto: and tel: prepended links not opening
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url == null || url.startsWith("http://") || url.startsWith("https://"))
+                    return false;
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                view.getContext().startActivity(intent);
+                return true;
+            }
+        };
+    }
+
+
 }
